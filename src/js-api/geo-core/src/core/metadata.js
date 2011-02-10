642ff35ea4d13017f2f89a342911aa87e9ea7e75
warfares <rodolfo.barriga@gmail.com>
2011-02-10 18:59:09 -0300

    add JS API metadata proxy

---
+++ src/js-api/geo-core/src/core/metadata.js
@@ -0,0 +1,29 @@
+Ext.ns("Geo.core");
+Geo.core.Metadata = Ext.extend(Ext.util.Observable, {
+	constructor: function (config) {
+		this.addEvents(
+			'distinct_values'
+		);
+		Geo.core.Metadata.superclass.constructor.call(config);
+	}
+	,
+	getDistinctValues:function(name, layer, limit){
+
+		var p = {
+			name : name,
+			layer : layer,
+			limit : limit
+		};
+
+		Ext.Ajax.request({
+			url: Geo.UriTemplate.getUri('metadata_distinct_values', '?' + Ext.urlEncode(p)),
+			method: 'Get',
+			headers: { 'Content-Type': 'text/json' },
+			scope: this,
+			success: function (response, options) {
+				var result = Ext.util.JSON.decode(response.responseText);
+				this.fireEvent('distinct_values', result);
+			}
+		});
+	}
+});
