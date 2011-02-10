d2ae5dcaeef09466375d72c4ee70ba559f8f2a17
warfares <rodolfo.barriga@gmail.com>
2011-02-10 18:57:41 -0300

    added metadata rest end point

---
+++ src/server/lib/geo/rest/metadata.py
@@ -0,0 +1,15 @@
+from bottle import *
+from geo.model import *
+
+@route('/getlocations')
+#TODO FIX pagging 
+
+
+@route('/metadata/distinct_values')
+def distinct_values():
+	name = request.GET.get('name')
+	layer_name = request.GET.get('layer')
+	limit = request.GET.get('limit')
+	m = Metadata(name)
+	values = m.distinct_values(layer_name, limit)
+	return vo.collection(values, len(values))
