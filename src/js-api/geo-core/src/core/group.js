d5a5652372df4a459662ff54ff6e41deead7562f
warfares <rodolfo.barriga@gmail.com>
2011-02-18 11:59:28 -0300

    added group withinbboxjs api method

--- src/js-api/geo-core/src/core/group.js
+++ src/js-api/geo-core/src/core/group.js
@@ -4,6 +4,7 @@ Geo.core.Group = Ext.extend(Ext.util.Observable, {
 		this.addEvents(
 			'staticbbox',
 			'withinpoint',
+			'withinbbox',
 			'closestpoint'
 		);
 		Geo.core.Group.superclass.constructor.call(config);
@@ -44,6 +45,24 @@ Geo.core.Group = Ext.extend(Ext.util.Observable, {
 			success: function (response, options) {
 				var result = Ext.util.JSON.decode(response.responseText);
 				this.fireEvent('withinpoint', result.geoms);
+			}
+		});
+	}
+	,
+	getWithInBBox: function (layers, bbox) {
+		var params = {
+			layers: layers,
+			bbox: bbox
+		};
+		Ext.Ajax.request({
+			url: Geo.UriTemplate.getUri('group_within_bbox'),
+			method: 'Post',
+			headers: { 'Content-Type': 'text/json' },
+			jsonData: params,
+			scope: this,
+			success: function (response, options) {
+				var result = Ext.util.JSON.decode(response.responseText);
+				this.fireEvent('withinbbox', result.geoms);
 			}
 		});
 	}
